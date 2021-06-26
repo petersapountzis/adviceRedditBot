843fe09197fb8f971f82b1d57d189cbe8bacdc4b
Peter Sapountzis <85853280+petersapountzis@users.noreply.github.com>
2021-06-26 14:18:17 -0700

    Update SadRedditBot.py

--- SadRedditBot.py
+++ SadRedditBot.py
@@ -12,10 +12,10 @@ reddit = praw.Reddit(client_id='<clientKey>',
 
 subreddit = reddit.subreddit("ApplyingToCollege")
 
-sad_quotes = ["Sadness flies away on the wings of time. - Ean de La Fontaine",
-              "The walls we build around us to keep sadness out also keeps out the joy. - Jim Rohn",
-              "In deep sadness there is no place for sentimentality. - William S. Burroughs",
-              "Sadness is but a wall between two gardens. -Kahlil Gibran"]
+advice = ["Go touch some grass",
+              "Get some fresh air and reevaluate",
+              "I'm sure it'll all work out",
+              "Hang in there!"]
 
 
 #lists front page posts
@@ -34,6 +34,6 @@ for submission in subreddit.hot(limit=5):
             if " stressed " in comment_lower:
                print ("------------------------------------------")
                print (comment.body)
-               random_index = random.randint(0,len(sad_quotes) - 1) 
-               comment.reply(sad_quotes[random_index])
+               random_index = random.randint(0,len(advice) - 1) 
+               comment.reply(advice[random_index])
                
